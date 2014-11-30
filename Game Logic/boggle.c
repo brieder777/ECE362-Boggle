24753f3a67df9b1a2e57ce02b35d0aa148fd592c
Subhav Ramachandran <rsubhav@users.noreply.github.com>
2014-11-30 15:29:05 -0500

    Some work filling in game-logic functions.

--- Game Logic/boggle.c
+++ Game Logic/boggle.c
@@ -7,11 +7,31 @@
 
 #include "boggle.h"
 
+/*
+ * Implementation Details for `generate_grid`
+ * ==========================================
+ * From http://everything2.com/title/Boggle, the letter distribution of Boggle
+ * dice is (out of 96):
+ * 
+ * A-6    H-5    O-7    V-2
+ * B-2    I-6    P-2    W-3
+ * C-2    J-1    Qu-1   X-1
+ * D-3    K-1    R-5    Y-3
+ * E-11   L-4    S-6    Z-1
+ * F-2    M-2    T-9
+ * G-2    N-6    U-3
+ * 
+ * Let's simplify this. Maybe split this into 3 groups?:
+ *		Vowels - A, E, I, O, U:		33 / 96
+ *		Rare   - J, K, Q, X, Z:		 5 / 96
+ *		Common Consonants:			58 / 96
+ */
 void generate_grid(unsigned int seed)
 {
 	char i, j;
+	char val;
 	
-	srand(seed); // Seed the included random number generator.
+	srand(seed); // Seed the random number generator.
 	
 	for(i = 0; i < BOGGLE_SIZE; i++)
 		for(j = 0; j < BOGGLE_SIZE; j++)
@@ -33,6 +53,8 @@ void generate_grid(unsigned int seed)
 static char is_adjacent(char* word, char grid[BOGGLE_SIZE][BOGGLE_SIZE],
 		char i, char j)
 {
+	char x, y;
+	
 	// If we've reached the end of the word, it hasn't been found.
 	if(*word == '\0')
 		return 0;
@@ -41,7 +63,15 @@ static char is_adjacent(char* word, char grid[BOGGLE_SIZE][BOGGLE_SIZE],
 	grid[i, j] = '\0';
 	
 	// Look through all valid surrounding locations (potentially 8).
+	for(x = i - 1; x <= i + 1 && x >= 0 && x < BOGGLE_SIZE ; x++)
+		for(y = i - 1; y <= i + 1 && y >= 0 && y < BOGGLE_SIZE ; y++)
+		{
+			if(*word == grid[x][y])
+				if(is_adjacent(word + 1, grid, x, y))
+					return 1;
+		}
 	
+	return 0;
 }
 
 char validate_word_grid(char* word)
