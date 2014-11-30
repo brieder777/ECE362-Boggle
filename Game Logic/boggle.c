e7b540bb6f1d175ffdd64340231ad8727e34aa27
Subhav Ramachandran <rsubhav@users.noreply.github.com>
2014-11-29 20:17:41 -0500

    Lots of misc. game logic stuff.
    None of this code has been tested.

--- Game Logic/boggle.c
+++ Game Logic/boggle.c
@@ -9,10 +9,68 @@
 
 void generate_grid(unsigned int seed)
 {
+	char i, j;
+	
+	srand(seed); // Seed the included random number generator.
+	
+	for(i = 0; i < BOGGLE_SIZE; i++)
+		for(j = 0; j < BOGGLE_SIZE; j++)
+		{
+			
+		}
+}
+
+/**
+ * Helper function to `validate_word_grid`. Finds out whether a word can be
+ * produced from a given starting location (i, j). 
+ * 
+ * @param word		Rest of word to find
+ * @param grid		Temporary grid containing NULL chars. in visited spaces.
+ * @param i			Row
+ * @param j			Column
+ * @return			1 if valid, 0 otherwise.
+ */
+static char is_adjacent(char* word, char grid[BOGGLE_SIZE][BOGGLE_SIZE],
+		char i, char j)
+{
+	// If we've reached the end of the word, it hasn't been found.
+	if(*word == '\0')
+		return 0;
+	
+	// Mark current location as visited.
+	grid[i, j] = '\0';
+	
+	// Look through all valid surrounding locations (potentially 8).
 	
 }
 
-void validate_word_grid(char* word)
+char validate_word_grid(char* word)
 {
+	char i, j;
+	char temp_grid[BOGGLE_SIZE][BOGGLE_SIZE];
 	
+	// Copy the grid to temp_grid.
+	for(i = 0; i < BOGGLE_SIZE; i++)
+		for(j = 0; j < BOGGLE_SIZE; j++)
+			temp_grid[i][j] = boggle_grid[i][j];
+	
+	// Look for possible first letters.
+	for(i = 0; i < BOGGLE_SIZE; i++)
+		for(j = 0; j < BOGGLE_SIZE; j++)
+			if(*word == temp_grid[i][j])
+			{
+				// Recurse to find possible second, third, etc. letters
+				if(is_adjacent(word + 1, temp_grid, i, j))
+					return 1;
+			}
+	
+	return 0;
+}
+
+char calculate_points(char* word)
+{
+	char points = 0;
+	
+	
+	return points;
 }
--- Game Logic/boggle.h
+++ Game Logic/boggle.h
@@ -17,11 +17,28 @@
 extern "C" {
 #endif
 
+#define BOGGLE_SIZE 4
+#define BOGGLE_WORDLEN 8
+#define BOGGLE_MAXWORDS 25
+
+/* 
+ * Information about a player.
+ * 
+ * This struct currently uses 401 bytes of memory.
+ * 
+ * @TODO We could potentially make this more space-effecient by putting all the
+ * words in one contiguous array, and delimiting them by null characters.
+ */
+typedef struct boggle_player {
+    char word_count = 0; // Number of words found.
+    char prev_words[BOGGLE_WORDLEN][BOGGLE_MAXWORDS]; // Array of previously found words.
+} Player;
+
 // Global letter grid variable.
 //
 // If we want to be able to use the letters QU, we would need to change this
 // structure.
-char boggle_grid[4][4];
+char boggle_grid[BOGGLE_SIZE][BOGGLE_SIZE];
 
 
 
@@ -44,7 +61,7 @@ void generate_grid(unsigned int seed);
  *                  a NULL character.)
  * @return          1 if valid, 0 otherwise
  */
-int validate_word_prev(char* word);
+char validate_word_prev(char* word, Player* player);
 
 /**
  * Validate an inputted word against the letter grid.
@@ -56,18 +73,37 @@ int validate_word_prev(char* word);
  *                  a NULL character.)
  * @return          1 if valid, 0 otherwise
  */
-int validate_word_grid(char* word);
+char validate_word_grid(char* word);
 
 /**
  * Calculate the number of points that a word is worth.
  * 
  * This does not validate the correctness of the word.
  * 
+ * By Boggle rules, scoring works as follows:
+ * <table>
+ *  <tr><th> No. of Letters </th>    <th> Points </th></tr>
+ *  <tr><td>              3 </td>    <td>      1 </td></tr>
+ *  <tr><td>              4 </td>    <td>      1 </td></tr>
+ *  <tr><td>              5 </td>    <td>      2 </td></tr>
+ *  <tr><td>              6 </td>    <td>      3 </td></tr>
+ *  <tr><td>              7 </td>    <td>      5 </td></tr>
+ *  <tr><td>             8+ </td>    <td>     11 </td></tr>
+ * </table>
+ * 
  * @param word      A string containing the word. (Terminated with a NULL
  *                  character.)
  * @return          The number of points the word is worth.
  */
 char calculate_points(char* word);
+
+/**
+ * Add a word to a player's list of previous words.
+ * 
+ * @param word      A string containing the word to add. (Terminated with a
+ *                  NULL character.)
+ */
+void Player_add_word(char* word);
 
 
 #ifdef	__cplusplus
