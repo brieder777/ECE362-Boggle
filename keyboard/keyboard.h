9fe8e67f7668da25a45d0fda38cd1e9f0f85c34a
Brian Rieder <brieder@purdue.edu>
2014-12-06 12:26:18 -0500

    Initial header commit -- only key defines

---
+++ keyboard/keyboard.h
@@ -0,0 +1,38 @@
+/* 
+ * File:   keyboard.h
+ * Author: Brian Rieder <brieder@purdue.edu>
+ *
+ * Created on December 6, 2014, 12:13 PM
+ * 
+ * ============================================================================
+ * 
+ * This library was made to be compatible with the CodeWarrior compiler.
+ */
+
+ #define KEYBOARD_A 0x1C
+ #define KEYBOARD_B 0x32
+ #define KEYBOARD_C 0x21
+ #define KEYBOARD_D 0x23
+ #define KEYBOARD_E 0x24
+ #define KEYBOARD_F 0x2B
+ #define KEYBOARD_G 0x34
+ #define KEYBOARD_H 0x33
+ #define KEYBOARD_I 0x43
+ #define KEYBOARD_J 0x3B
+ #define KEYBOARD_K 0x42
+ #define KEYBOARD_L 0x4B
+ #define KEYBOARD_M 0x3A
+ #define KEYBOARD_N 0x31
+ #define KEYBOARD_O 0x44
+ #define KEYBOARD_P 0x4D
+ #define KEYBOARD_Q 0x15
+ #define KEYBOARD_R 0x2D
+ #define KEYBOARD_S 0x1B
+ #define KEYBOARD_T 0x2C
+ #define KEYBOARD_U 0x3C
+ #define KEYBOARD_V 0x2A
+ #define KEYBOARD_W 0x1D
+ #define KEYBOARD_X 0x22
+ #define KEYBOARD_Y 0x35
+ #define KEYBOARD_Z 0x1A
+ #define KEYBOARD_ENTER 0x5A
