6fe1851297e7c9b41b48f15cfa1225690b0dfff2
Subhav Ramachandran <rsubhav@users.noreply.github.com>
2014-12-07 18:13:11 -0500

    Renamed our termio file.
    We were having conflicts with the one in the CodeWarrior library.

---
+++ course_includes/our_termio.c
@@ -0,0 +1,72 @@
+/* 
+ * File:   our_termio.c
+ * Author: Subhav Ramachandran <subhav@purdue.edu>
+ *
+ * Created on October 31, 2014, 11:30 AM
+ */
+
+#include <hidef.h>      /* common defines and macros */
+#include "derivative.h"      /* derivative-specific definitions */
+#include <mc9s12c32.h>
+
+#include "our_termio.h"
+
+char inchar(void)
+{
+	/* receives character from the terminal channel */
+	while(!(SCISR1 & 0x20)); /* wait for input */
+	return SCIDRL;
+}
+
+void outchar(char x)
+{
+	/* sends a character to the terminal channel */
+	while(!(SCISR1 & 0x80)); /* wait for output buffer empty */
+	SCIDRL = x;
+}
+
+void outbin_int(int x)
+{
+  unsigned char itr;
+  for(itr = 0; itr < 16; ++itr) {
+    outchar((x & 0x01) + '0');
+    x = x >> 1;
+  }
+  outchar('\r');
+  outchar('\n');
+}
+
+void outbin(char x)
+{
+  unsigned char itr;
+  for(itr = 0; itr < 8; ++itr) {
+    outchar((x & 0x01) + '0');
+    x = x >> 1;
+  }
+  outchar('\r');
+  outchar('\n');
+}
+
+void outstr(char* str)
+{
+	while(*str != '\0')
+	{
+		bco(*str);
+		str++;
+	}
+}
+
+void outnum(unsigned int number, unsigned char digits)
+{
+	char string[6];
+	
+	string[digits] = '\0';
+	
+	while(digits > 0)
+	{
+		string[--digits] = (number % 10) + '0';
+		number /= 10;
+	}
+	
+	pmsg(string);
+}
---
+++ course_includes/our_termio.h
@@ -0,0 +1,67 @@
+/* 
+ * File:   termio.h
+ * Author: Subhav Ramachandran <subhav@purdue.edu>
+ *
+ * Created on October 31, 2014, 11:28 AM
+ */
+
+#ifndef OUR_TERMIO_H
+#define	OUR_TERMIO_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*
+ ***********************************************************************
+ Character I/O Library Routines for 9S12C32 (for debugging only)
+ ***********************************************************************
+ Name:         inchar
+ Description:  inputs ASCII character from SCI serial port and returns it
+ Example:      char ch1 = inchar();
+ ***********************************************************************
+ */
+char inchar(void);
+
+/*
+ ***********************************************************************
+ Name:         outchar
+ Description:  outputs ASCII character x to SCI serial port
+ Example:      outchar('x');
+ ***********************************************************************
+ */
+void outchar(char x);
+
+/**
+ * General purpose debugging function that writes a character in binary
+ * to the SCI serial port.
+ *
+ */
+void outbin(char x);
+
+/**
+ * General purpose debugging function that writes an integer in binary
+ * to the SCI serial port.
+ *
+ */
+void outbin_int(int x);
+
+/**
+ * Output a string to an SCI terminal.
+ * @param str
+ */
+void outstr(char* str);
+
+/**
+ * Output a number to an SCI terminal.
+ * @param number
+ * @param digits
+ */
+void outnum(unsigned int number, unsigned char digits);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* OUR_TERMIO_H */
+
